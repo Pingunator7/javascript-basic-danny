fa60b4f903702cb6d6bbb3913eb363cc39345153
DANNY <est.djmoreno0201@uml.edu.ni>
2025-03-30 00:09:23 -0600

    ejercicio 1

---
+++ pares.js
@@ -0,0 +1,17 @@
+console.log("el valor limite es" , 650)
+
+const numero = parseInt(50)
+
+
+
+if(numero>650){
+
+    console.log("Has ingresado un valor + de 650")
+
+
+
+}else{
+
+    console.log("el numero es par de 50")
+
+}
