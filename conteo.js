309c6736f51cff39cf321930a2c804bc5b7b2a52
DANNY <est.djmoreno0201@uml.edu.ni>
2025-03-30 00:10:52 -0600

    ejercicio 3

---
+++ conteo.js
@@ -0,0 +1,8 @@
+console.log("ingrese el numero inicial del rango : 10")
+const numero = parseInt ("rango")
+if (numero>50){
+    console.log("cantidad de numeros pares :21")
+    console.log("cantidad de numeros impares :20")
+}else{
+console.log("el numero de rango es del 10 al 50")
+}
